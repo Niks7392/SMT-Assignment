d381aadac377e80016adad9708af041f50557db5
niks7392 <nikantkamat99@gmail.com>
2022-05-12 12:06:24 +0530

    solved errors

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -7,7 +7,7 @@ export class Products extends Component {
   }
 
   render() {
-    let { title, description, imageUrl, productId } = this.props;
+    let { title, description, imageUrl, } = this.props;
     return (
       <div className='container mainCard my-3'>
         <div className="card" style={{ width: '18rem' }}>
@@ -15,7 +15,7 @@ export class Products extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href='#'  onClick={this.alertShow} className="btn btn-primary">Buy Now</a>
+            <a href='/'  onClick={this.alertShow} className="btn btn-primary">Buy Now</a>
           </div>
         </div>
       </div>
